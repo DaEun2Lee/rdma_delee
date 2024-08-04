2b5f6f5efed3138a5f511656baaf503b152dc643
DaEun2Lee <delee0929@gmail.com>
2024-08-04 05:12:22 +0000

    MOD: .

--- rdma/test_speed/simple_wget.sh
+++ rdma/test_speed/simple_wget.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 #for i in {1..100}; do
-#  echo "Running iteration $i"    
+  echo "Running iteration $i"    
 start_time=$(date +%s.%N)    
 #  sudo sshpass -f ./passwd_ubuntu.txt scp -P 3300 ubuntu@10.20.16.66:/home/ubuntu/test/Bible.pdf .    
 wget 10.20.16.78:80/index.html
 end_time=$(date +%s.%N)  
 execution_time=$(echo "$end_time - $start_time" | bc)    
-#  file_size=$(du -h The_Little_Prince.txt | cut -f1)    
-#  echo "Result for iteration $i: Exit code: $? | Execution time: $execution_time seconds | File size: $file_size" >> results.log
+  file_size=$(du -h index.html | cut -f1)    
+echo "Result for iteration $i: Exit code: $? | Execution time: $execution_time seconds | File size: $file_size" >> results.log
 #done
